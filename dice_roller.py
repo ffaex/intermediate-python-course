99be90c6777e3d5c49f59525a89c8f683e149c2d
ff <fyodor.fomichev@gmail.com>
2020-09-19 14:18:42 +0200

    dice roller

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
+
 def main():
-  print('You rolled a die')
+	roll = random.randint(1, 6)
+	print(f"You rolled a {roll}")
+
+
+
+
+	print('You rolled a die')
 
 if __name__== "__main__":
   main()
